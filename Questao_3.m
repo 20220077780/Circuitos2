259dfd0f713bae86254db456e0fc474223996ca5
20220077780 <gabriel.pessoa@estudante.cear.ufpb.br>
2024-08-19 21:51:00 -0300

    Add files via upload

--- Questao_3.m
+++ Questao_3.m
@@ -10,24 +10,25 @@ i=1;
 A = [1 1 1 1];
 At = transpose(A); 
 
-Js = 0; 
-Vs= [(-v/s)+(2/s)+1; -(l*i+4/s); -4/s; 0]; 
+Js = 0;
+Vs= [(-v/s)+(2/s+1); -(l*i+4/s); -4/s; 0]; 
 Yb = [
     s*c 0 0 0;
     0 1 0 0;
     0 0 1/(s*l) 0;
     0 0 0 1/3
-    ]
+    ];
 
-Yn = A*Yb*transpose(A)
-Is = -A*Js + A*Yb*Vs
+Yn = A*Yb*transpose(A);
+Is = -A*Js+A*Yb*Vs;
 
 %   Tensões de nó (E), tensões de ramo (V) e correntes de ramo (J):
 
-E = inv(Yn)*Is
-V = transpose(A)*E
-J = Js + Yb*V - Yb*Vs;
+E = inv(Yn)*Is;
+V = transpose(A)*E;
+%J = Js+Yb*V-Yb*Vs;
 
-figure;
-impulse(E(2,1));
+%figure
+%impulse(E(2,1))
+impulse(E)
 title('Resposta ao Impulso da Tensão do Nó 02');
